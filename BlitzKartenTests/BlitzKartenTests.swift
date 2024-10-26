06f16e9f4c2a2fbb99122b2599b079e5355039ba
Matt Cooper <mccoopton@gmail.com>
2024-10-25 18:46:07 -0600

    Initial Commit

---
+++ BlitzKartenTests/BlitzKartenTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlitzKartenTests.swift
+//  BlitzKartenTests
+//
+//  Created by Brenna Cooper on 10/25/24.
+//
+
+import Testing
+@testable import BlitzKarten
+
+struct BlitzKartenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
